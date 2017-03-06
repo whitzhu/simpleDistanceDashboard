bff4a9103dce6cdde21059c24d6b0fdf440a0774
whitzhu <whitzhu@gmail.com>
2017-03-05 18:03:08 -0800

    attempting to switch to @googlemap api

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,17 +1,42 @@
 var express = require('express');
 var router = require('express').Router();
 var distance = require('google-distance');
+var Googlekey = require('../../env/googlekey');
+var googleMapsClient = require('@google/maps').createClient({
+  key: Googlekey
+});
 
 
 var reqData;
 
 router.get('/', function(req, res) {
+/*  googleMapsClient.distanceMatrix({
+    origins: ['San Francisco, CA', 'Seattle'],
+    destinations: ['New York'],
+    mode: 'driving'
+  }, 
+  function(err, data) {
+    if (err) return console.log(err);
+    console.log('got post request, processing get, about to post');
+    res.end(JSON.stringify(data.json, null, 2));
+  })*/
   
-})
+});
 
 router.post('/', function(req, res) {
 
-  console.log(req.body);
+  googleMapsClient.distanceMatrix({
+    origins: ['San Francisco, CA'],
+    destinations: ['New York'],
+    mode: 'driving'
+  }, 
+  function(err, data) {
+    if (err) return console.log(err);
+    console.log('got post request, processing get, about to post');
+    res.json(data.json);
+  })
+
+  /*console.log(req.body);
   distance.get(
     {
       origin: req.body.origin,
@@ -23,7 +48,7 @@ router.post('/', function(req, res) {
       if (err) return console.log(err);
       console.log('got post request, processing get, about to post');
       res.json(data);
-  });
+  });*/
 })
 
 
